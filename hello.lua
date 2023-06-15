7f335421d68ea453383ac720c4d630c1d0c3bff6
Ehsan Mohammadi <ehsanx64@gmail.com>
2023-06-15 22:19:27 +0330

    Add simple scripts

---
+++ hello.lua
@@ -0,0 +1,4 @@
+-- The first program in every language
+-- Run it with: lua hello.lua
+
+io.write("Hello world, from ", _VERSION, "!\n")
---
+++ printf.lua
@@ -0,0 +1,8 @@
+-- An implementation of printf
+-- To run: lua printf.lua
+
+function printf(...)
+ io.write(string.format(...))
+end
+
+printf("Hello %s from %s on %s\n", os.getenv"USER" or "there", _VERSION,os.date())
